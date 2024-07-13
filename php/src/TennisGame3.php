8951977f838173f5e5caa4f80c0a1ad5635822dd
Fuen Sansano <fuensansano@gmail.com>
2024-07-13 18:34:36 +0200

    extract variables and rename them

--- php/src/TennisGame3.php
+++ php/src/TennisGame3.php
@@ -4,38 +4,40 @@ namespace TennisGame;
 
 class TennisGame3 implements TennisGame
 {
-    private $p2 = 0;
-    private $p1 = 0;
-    private $p1N = '';
-    private $p2N = '';
+    const ADVANTAGEPOINT = 1;
+    private $points =["Love", "Fifteen", "Thirty", "Forty"];
+    private $scorePlayer1 = 0;
+    private $scorePlayer2 = 0;
+    private $player1Name = '';
+    private $player2Name = '';
+    private  const DEUCE  = "Deuce";
 
-    public function __construct($p1N, $p2N)
+    public function __construct($player1Name, $player2Name)
     {
-        $this->p1N = $p1N;
-        $this->p2N = $p2N;
+        $this->player1Name = $player1Name;
+        $this->player2Name = $player2Name;
     }
 
     public function getScore()
     {
-        if ($this->p1 < 4 && $this->p2 < 4 && !($this->p1 + $this->p2 == 6)) {
-            $p = array("Love", "Fifteen", "Thirty", "Forty");
-            $s = $p[$this->p1];
-            return ($this->p1 == $this->p2) ? "{$s}-All" : "{$s}-{$p[$this->p2]}";
+        if ($this->scorePlayer1 < 4 && $this->scorePlayer2 < 4 && !($this->scorePlayer1 + $this->scorePlayer2 == 6)) {
+            $set = $this->points[$this->scorePlayer1];
+            return ($this->scorePlayer1 == $this->scorePlayer2) ? "{$set}-All" : "{$set}-{$this->points[$this->scorePlayer2]}";
         } else {
-            if ($this->p1 == $this->p2) {
-                return "Deuce";
+            if ($this->scorePlayer1 == $this->scorePlayer2) {
+                return self::DEUCE;
             }
-            $s = $this->p1 > $this->p2 ? $this->p1N : $this->p2N;
-            return (($this->p1 - $this->p2) * ($this->p1 - $this->p2) == 1) ? "Advantage {$s}" : "Win for {$s}";
+            $set = $this->scorePlayer1 > $this->scorePlayer2 ? $this->player1Name : $this->player2Name;
+            return (($this->scorePlayer1 - $this->scorePlayer2) * ($this->scorePlayer1 - $this->scorePlayer2) == self::ADVANTAGEPOINT) ? "Advantage {$set}" : "Win for {$set}";
         }
     }
 
     public function wonPoint($playerName)
     {
         if ($playerName == "player1") {
-            $this->p1++;
+            $this->scorePlayer1++;
         } else {
-            $this->p2++;
+            $this->scorePlayer2++;
         }
     }
 
