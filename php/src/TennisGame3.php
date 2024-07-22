3fb1ee8e978cf17d57e2dc8478a153dda175f8f0
Fuen Sansano <fuensansano@gmail.com>
2024-07-22 18:52:06 +0200

    REFACTORS: type methods, extracts constant

--- php/src/TennisGame3.php
+++ php/src/TennisGame3.php
@@ -4,22 +4,23 @@ namespace TennisGame;
 
 class TennisGame3 implements TennisGame
 {
-    const ADVANTAGEPOINT = 1;
+    private const ADVANTAGE_POINT = 1;
     private const MAX_POINTS_BEFORE_ADVANTAGE = 4;
-    private  const DEUCE  = "Deuce";
-    private $points =["Love", "Fifteen", "Thirty", "Forty"];
-    private $scorePlayer1 = 0;
-    private $scorePlayer2 = 0;
-    private $player1Name = '';
-    private $player2Name = '';
+    private const DEUCE = "Deuce";
+    private const POINTS = ["Love", "Fifteen", "Thirty", "Forty"];
+    private const EARLY_SUM_POINTS = 6;
+    private int $scorePlayer1 = 0;
+    private int $scorePlayer2 = 0;
+    private string $player1Name;
+    private string $player2Name;
 
-    public function __construct($player1Name, $player2Name)
+    public function __construct(string $player1Name, string $player2Name)
     {
         $this->player1Name = $player1Name;
         $this->player2Name = $player2Name;
     }
 
-    public function getScore()
+    public function getScore(): string
     {
         if ($this->isEarlyGameStage()) {
             return $this->extractGameScore();
@@ -30,18 +31,19 @@ class TennisGame3 implements TennisGame
         return $this->retrieveWinPlayerName();
     }
 
-    public function wonPoint($playerName)
+    public function wonPoint(string $playerName): void
     {
-        if ($playerName == "player1") {
+        if ($playerName === $this->player1Name) {
             $this->scorePlayer1++;
-        } else {
+        }
+        if ($playerName === $this->player2Name) {
             $this->scorePlayer2++;
         }
     }
 
     private function isADraw(): bool
     {
-        return $this->scorePlayer1 == $this->scorePlayer2;
+        return $this->scorePlayer1 === $this->scorePlayer2;
     }
 
     private function extractAdvantagePoint(): int
@@ -60,24 +62,25 @@ class TennisGame3 implements TennisGame
     private function retrieveWinPlayerName(): string
     {
         $set = $this->extractAdvantagePlayerName();
-        return $this->isOneAdvantagePoint() ? "Advantage {$set}" : "Win for {$set}";
+        return $this->isOneAdvantagePoint() ? "Advantage $set" : "Win for $set";
     }
 
     private function isOneAdvantagePoint(): bool
     {
-        return (($this->extractAdvantagePoint()) * ($this->extractAdvantagePoint()) == self::ADVANTAGEPOINT);
+        return (($this->extractAdvantagePoint()) * ($this->extractAdvantagePoint()) === self::ADVANTAGE_POINT);
     }
 
-    private function isEarlyGameStage():bool
+    private function isEarlyGameStage(): bool
     {
-        return $this->scorePlayer1 < self::MAX_POINTS_BEFORE_ADVANTAGE &&
-            $this->scorePlayer2 < self::MAX_POINTS_BEFORE_ADVANTAGE
-            && !($this->scorePlayer1 + $this->scorePlayer2 == 6);
+        return $this->scorePlayer1 < self::MAX_POINTS_BEFORE_ADVANTAGE
+            && $this->scorePlayer2 < self::MAX_POINTS_BEFORE_ADVANTAGE
+            && !($this->scorePlayer1 + $this->scorePlayer2 === self::EARLY_SUM_POINTS);
     }
 
     private function extractGameScore(): string
     {
-        $set = $this->points[$this->scorePlayer1];
-        return ($this->isADraw()) ? "{$set}-All" : "{$set}-{$this->points[$this->scorePlayer2]}";
+        $set1 = self::POINTS[$this->scorePlayer1];
+        $set2 = self::POINTS[$this->scorePlayer2];
+        return ($this->isADraw()) ? "$set1-All" : "$set1-$set2";
     }
 }
